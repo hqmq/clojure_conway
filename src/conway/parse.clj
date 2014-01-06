5dfce3d14c51653d8a00eb243b72f8f0b18006ef
Michael Ries <michael@riesd.com>
2014-01-06 15:08:33 -0700

    rendering a grid down to a printable string

--- src/conway/parse.clj
+++ src/conway/parse.clj
@@ -6,7 +6,18 @@
     (= char \o) :alive
     :else :dead))
 
+(defn cell-to-character [cell]
+  (cond
+    (= cell :alive) \o
+    :else \.))
+
 (defn parse [board_string]
   (vec (map (fn [line]
     (vec (map character-to-cell line)))
       (str/split-lines board_string))))
+
+(defn render [grid]
+  (str/join "\n"
+    (map (fn [row]
+      (apply str (map cell-to-character row)))
+      grid)))
--- test/conway/parse_test.clj
+++ test/conway/parse_test.clj
@@ -3,8 +3,8 @@
             [conway.parse :as p]))
 
 (def board
-"  o  
-o o o")
+"..o..
+o.o.o")
 
 (def grid [[:dead :dead :alive :dead :dead] [:alive :dead :alive :dead :alive]])
 
@@ -13,3 +13,7 @@ o o o")
     (is (= grid (p/parse board))))
   (testing "Handles trailing newlines"
     (is (= grid (p/parse (str board "\n"))))))
+
+(deftest board-render-test
+  (testing "a grid can be rendered down to a printable string"
+    (is (= board (p/render grid)))))
